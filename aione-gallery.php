ecc336ae8ed4dbd441efe77875fc44a6a5c42a02
SGS Sandhu <contact@sgssandhu.com>
2017-04-12 10:40:16 +0530

    Version Updated. Git URI Changed

--- aione-gallery.php
+++ aione-gallery.php
@@ -16,13 +16,15 @@
  * Plugin Name:       Aione Gallery
  * Plugin URI:        http://oxosolutions.com/products/wordpress-plugins/aione-gallery
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
+ * Version:           2.0.0.1
  * Author:            SGS Sandhu
  * Author URI:        http://sgssandhu.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       aione-gallery
  * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/oxosolutions/aione-gallery
+ * GitHub Branch: master
  */
 
 // If this file is called directly, abort.
